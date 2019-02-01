f9e5bc36f298955b337db740b581ef1a168e4641
R Zhang <detchue@gmail.com>
2019-02-01 17:20:35 -0500

    no message

--- main_190201.py
+++ main_190201.py
@@ -19,34 +19,24 @@ import random
 import pyexcel as pe
 '''
 @Ruiyu
-2019.01.30
-ToDo: Text branch only, hi mode RNN
+2019.02.01
+ToDo: Text branch only, hi mode RNN -> Attach result to mid mode RNN
 '''
 ## TRAING PARAMS
 batch_size = 32
-epoch_count = 10
+epoch_count_1 = 10
+epoch_count_2 = 10
 acc_flag_threshould = 60 # threshould of flag to detect in-training effects, not must
 acc_collection = [] # all accuracies
 work_path = '/Volumes/Detchue Base II/731/CNMC/hospital_data'
 saving_path = '/Volumes/Detchue Base II/731/CNMC'
 saving_name = ['/result/train_text.mat', '/result/test_text.mat']
-label_mode = 'h'
 
 ## LOAD DATA
 cirno = data(path = work_path) # all train/test data
 cirno.auto_process(merge_unclear = True)
-cirno.label_mode = label_mode
-if label_mode == 'lower_10':
-    numclass = 11
-elif label_mode == 'h':
-    numclass = len(cirno.label_dic_h)
-elif label_mode == 'm':
-    numclass = len(cirno.label_dic_m)
-elif label_mode == 'l':
-    numclass = len(cirno.label_dic_l)
-else:
-    numclass = len(cirno.trainer_lbl_statistics) - len(cirno.unclear_lbl) + 4
-    print('>!>Warning, unknown label mode')
+cirno.label_mode = 'h'
+numclass = len(cirno.label_dic_h)
 
 ## IN-TRAINING FUNCTIONS
 def output_result(train_text, test_text):
@@ -69,7 +59,7 @@ def to_one_digit_label(onehot_labels):
     for label in onehot_labels: res.append(np.argmax(label))
     return res
 
-## TEXT MODEL
+## TEXT MODEL HI-LEVEL-LABEL
 # input and its shape
 text_input = Input(shape = (30,))
 # word embedding
@@ -108,11 +98,52 @@ adam = Adam(lr = 0.0001, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08)
 text_model.compile(loss = 'categorical_crossentropy', optimizer = adam, metrics = ['accuracy'])
 text_model.summary()
 
+## TEXT MODEL MID-LEVEL-LABEL
+numclass = len(cirno.label_dic_m)
+# input and its shape
+text_input_2 = Input(shape = (30,))
+# word embedding
+em_text_2 = Embedding(len(cirno.word_dic) + 1,
+                    200,
+                    weights = [cirno.get_embed_matrix()],
+                    trainable = True)(text_input_2)
+# masking layer
+text_2 = Masking(mask_value = 0.)(em_text_2)
+# LSTM layer
+text_2 = LSTM(512,
+            return_sequences = True,
+            recurrent_dropout = 0.25,
+            name = 'LSTM_text_1')(text_2)
+text_2 = LSTM(256,
+            return_sequences = True,
+            recurrent_dropout = 0.25,
+            name = 'LSTM_text_2')(text_2)
+# batch normalization
+#text_l1 = BatchNormalization()(text_l1)
+# attention layer
+text_weight_2 = AttentionLayer()(text_2)
+text_weight_2 = Lambda(weight_expand)(text_weight_2)
+text_vector_2 = Lambda(weight_dot)([text_2, text_weight_2])
+text_feature_vector_2 = Lambda(lambda x: backend.sum(x, axis = 1))(text_vector_2)
+# dropout layer
+dropout_text_2 = Dropout(0.25)(text_feature_vector_2)
+dense_text_1_2 = Dense(128, activation = 'relu')(dropout_text_2)
+dropout_text_2 = Dropout(0.25)(dense_text_1_2)
+# decision-making
+text_prediction_2 = Dense(numclass, activation = 'softmax')(dropout_text_2)
+text_model_2 = Model(inputs = text_input_2, outputs = text_prediction_2)
+inter_text_2 = Model(inputs = text_input_2, outputs = text_feature_vector_2)
+# optimizer
+adam = Adam(lr = 0.0001, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08)
+text_model_2.compile(loss = 'categorical_crossentropy', optimizer = adam, metrics = ['accuracy'])
+text_model_2.summary()
+
 ## MAIN
 if __name__ == "__main__":
+    # HI-LEVEL-LABEL MODE TRAINING
     acc_max = 0
-    for i in range(epoch_count):
-        print('\n\n>>>High-level-label Text Training Epoch: ' + str(i) + ' out of ' + str(epoch_count))
+    for i in range(epoch_count_1):
+        print('\n\n>>>High-level-label Text Training Epoch: ' + str(i + 1) + ' out of ' + str(epoch_count_1))
         # get data
         test_label, test_text, test_audio_left, test_audio_right = cirno.get_tester(average = True)
         train_label, train_text, train_audio_left, train_audio_right = cirno.get_trainer(average = True)
@@ -143,3 +174,13 @@ if __name__ == "__main__":
 
     print('\n\n>>>High-level-label Training All Done')
     print('>Max Text Acc =', acc_max)
+
+    # MID-LEVEL-LABEL MODE TRAINING
+    cirno.label_mode = 'm'
+    text_model.load_weights(saving_path + 'entire_text_output_weights.h5')
+    acc_max = 0
+    for i in range(epoch_count_2):
+        print('\n\n>>>Mid-level-label Text Training Epoch: ' + str(i + 1) + ' out of ' + str(epoch_count_2))
+        # get data
+
+
